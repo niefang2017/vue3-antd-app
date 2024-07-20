35f1a4667958ab2e4da533bf8ea8bc43f2a9fe5f
聂芳 <nie4161204@qq.com>
2024-07-21 04:13:19 +0800

    ci(custom): 更改提交配置

--- .cz-config.cjs
+++ .cz-config.cjs
@@ -1,58 +1,65 @@
 module.exports = {
-    types: [
-      {
-        value: '✨ feat: ',
-        name: '✨ feat:     新功能'
-      },
-      {
-        value: '🐛 fix:',
-        name: '🐛 fix:      修复bug'
-      },
-      {
-        value: '📦️ build:',
-        name: '📦️ build:    打包'
-      },
-      {
-        value: '⚡️ perf:',
-        name: '⚡️ perf:     性能优化'
-      }, {
-        value: '🎉 release:',
-        name: '🎉 release:  发布正式版'
-      }, {
-        value: '💄 style:',
-        name: '💄 style:    代码的样式美化'
-      }, {
-        value: '♻️  refactor:',
-        name: '♻️  refactor: 重构'
-      }, {
-        value: '✏️  docs:',
-        name: '✏️  docs:     文档变更'
-      }, {
-        value: '✅ test:',
-        name: '✅ test:     测试'
-      }, {
-        value: '⏪️ revert:',
-        name: '⏪️ revert:   回退'
-      }, {
-        value: '🚀 chore:',
-        name: '🚀 chore:    构建/工程依赖/工具'
-      }, {
-        value: '👷 ci:',
-        name: '👷 ci:       CI related changes'
-      }
-    ],
-    messages: {
-      type: '请选择提交类型(必填)',
-      customScope: '请输入文件修改范围(可选)',
-      subject: '请简要描述提交(必填)',
-      body: '请输入详细描述(可选)',
-      breaking: '列出任何BREAKING CHANGES(可选)',
-      footer: '请输入要关闭的issue(可选)',
-      confirmCommit: '确定提交此说明吗？'
+  types: [
+    {
+      value: 'feat',
+      name: '✨ feat:     新功能'
     },
-    allowCustomScopes: true, 
-    // 跳过问题
-    skipQuestions: ['body', 'footer'],
-    subjectLimit: 72
-  }
-  
+    {
+      value: 'fix',
+      name: '🐛 fix:      修复bug'
+    },
+    {
+      value: 'build',
+      name: '📦️ build:    打包'
+    },
+    {
+      value: 'perf',
+      name: '⚡️ perf:     性能优化'
+    },
+    {
+      value: 'release',
+      name: '🎉 release:  发布正式版'
+    },
+    {
+      value: 'style',
+      name: '💄 style:    代码的样式美化'
+    },
+    {
+      value: 'refactor',
+      name: '♻️  refactor: 重构'
+    },
+    {
+      value: 'docs',
+      name: '✏️  docs:     文档变更'
+    },
+    {
+      value: 'test',
+      name: '✅ test:     测试'
+    },
+    {
+      value: 'revert',
+      name: '⏪️ revert:   回退'
+    },
+    {
+      value: 'chore',
+      name: '🚀 chore:    构建/工程依赖/工具'
+    },
+    {
+      value: 'ci',
+      name: '👷 ci:       CI相关更改'
+    }
+  ],
+  messages: {
+    type: '请选择提交类型(必填)',
+    customScope: '请输入文件修改范围(可选)',
+    subject: '请简要描述提交(必填)',
+    body: '请输入详细描述(可选)',
+    breaking: '列出任何BREAKING CHANGES(可选)',
+    footer: '请输入要关闭的issue(可选)',
+    confirmCommit: '确定提交此说明吗？'
+  },
+  allowCustomScopes: true,
+  // 跳过问题
+  skipQuestions: ['body', 'footer'],
+  subjectLimit: 72
+}
--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,54 @@
 module.exports = {
-  extends: ['cz']
+  extends: ['@commitlint/config-conventional', 'cz'],
+  // rules: {
+  //   'type-enum': [
+  //     2,
+  //     'always',
+  //     [
+  //       'feat', // 新功能
+  //       '🐛 fix', // 修复bug
+  //       '📦️ build:', // 打包
+  //       '⚡️ perf:', // 性能优化
+  //       '🎉 release:', // 发布正式版
+  //       '💄 style:', // 代码的样式美化
+  //       '♻️  refactor:', // 重构
+  //       '✏️  docs:', // 文档变更
+  //       '✅ test:', // 测试
+  //       '⏪️ revert:', // 回退
+  //       '🚀 chore:', // 构建/工程依赖/工具
+  //       '👷 ci:' // CI related changes
+  //     ]
+  //   ]
+  // }
+  prompt: {
+    questions: [
+      {
+        type: 'input',
+        name: 'subject',
+        message: '请输入commit信息',
+        validate: function (value) {
+          if (value.length) {
+            return true
+          } else {
+            return '请输入commit信息'
+          }
+        }
+      }
+    ],
+    types: [
+      { value: 'feat', name: '✨ feat:     新功能', emoji: ':sparkles:' },
+      { value: 'fix', name: '🐛 fix:      修复bug', emoji: ':bug:' },
+      { value: 'docs', name: '✏️  docs:     文档变更', emoji: ':memo:' },
+      { value: 'style', name: '💄 style:    代码的样式美化', emoji: ':lipstick:' },
+      { value: 'refactor', name: '♻️  refactor: 重构', emoji: ':recycle:' },
+      { value: 'perf', name: '⚡️ perf:     性能优化', emoji: ':zap:' },
+      { value: 'release', name: '🎉 release:  发布正式版', emoji: ':tada:' },
+      { value: 'test', name: '✅ test:     测试', emoji: ':white_check_mark:' },
+      { value: 'build', name: '📦️ build:    打包', emoji: ':package:' },
+      { value: 'ci', name: '👷 ci:       CI相关更改', emoji: ':ferris_wheel:' },
+      { value: 'chore', name: '🚀 chore:    构建/工程依赖/工具', emoji: ':hammer:' },
+      { value: 'revert', name: '⏪️ revert:   回退', emoji: ':rewind:' }
+    ],
+    useEmoji: true
+  }
 }
